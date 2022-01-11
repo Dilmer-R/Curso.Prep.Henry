0d5da479c789ae38bcc8baf811d7afeb69e547ee
Dilmer-R <rojascastaneda893@gmail.com>
2022-01-11 01:27:08 +0100

    variables

---
+++ VARIABLES/variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Variables en python
+# Declaracion de variables 
+
+
+nombre = "Dilmer"
+edad = 18
+'''Imprimir nombre y edad'''
+print("Mi nombre es {} y tengo {} años de edad.".format(nombre, edad))
